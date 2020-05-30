0b1271a55d46e8ae4de26a0a4ce0dc05c2a29597
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-05-30 17:07:10 +0200

    Encode login information

--- elisp-matrix-test.el
+++ elisp-matrix-test.el
@@ -1,6 +1,12 @@
 (require 'buttercup)
 (require 'elisp-matrix)
+(require 'json)
 
 (describe "elisp-matrix"
-  (it "does something"
-    (expect nil)))
+  (describe "encode-login"
+    (it "creates the correct JSON"
+      (let* ((encoded-login (matrix/encode-login "user.name" "user.password"))
+             (decoded-login (json-read-from-string encoded-login)))
+        (expect (alist-get 'user decoded-login) :to-equal "user.name")
+        (expect (alist-get 'password decoded-login) :to-equal "user.password")
+        (expect (alist-get 'type decoded-login) :to-equal "m.login.password")))))
--- elisp-matrix.el
+++ elisp-matrix.el
@@ -1,3 +1,11 @@
+(require 'dash)
+(require 'json)
 
+(defconst login-endpoint "/_matrix/client/r0/login")
+
+(defun matrix/encode-login (username password)
+  (json-encode-plist (list "type" "m.login.password"
+                           "user" username
+                           "password" password)))
 
 (provide 'elisp-matrix)
